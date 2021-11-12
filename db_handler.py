271a4744096ed3287da872f25e56be8467ee090e
Partha Biswas <59937090+itzzpartha@users.noreply.github.com>
2021-11-12 12:12:41 +0530

    Delete db_handler.py

--- db_handler.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-import pandas as pd
-engine=sqlalchemy.create_engine('sqlite:///database.db')
-df=pd.read_csv('main.csv')
-df.to_sql('main',engine,if_exists='replace',index=False)
-df=pd.read_csv('open_positions.csv')
-df.to_sql('open_positions',engine,if_exists='replace',index=False)
-df=pd.read_csv('shortlist.csv')
-df.to_sql('shortlist',engine,if_exists='replace',index=False)
